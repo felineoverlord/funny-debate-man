120096e2f0f4e405e1ffcc303aa277cadb77f852
felineoverlord <felineoverlord42@gmail.com>
2022-10-13 23:41:11 -0700

    organize role and channel ids

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 // this line is required for when I run the bot locally for debugging
-// require('dotenv').config();
+require("dotenv").config();
 
-const { GatewayIntentBits} = require('discord.js');
+const { GatewayIntentBits} = require("discord.js");
 const Discord = require("discord.js");
 const client = new Discord.Client({
     intents: [
@@ -12,6 +12,21 @@ const client = new Discord.Client({
     ]
 });
 
+const emotes = {
+    panlijah: "<:Panlijah:1005333329660362762>"
+}
+
+const roles = {
+    self: "<@1024409224400351274>",
+    elijah: "<@&1024472122896167018>",
+    bensBoyfriend: "<@&1025084954352291950>"
+}
+
+const channels = {
+    rules: "<#968645464457101362>",
+    roles: "<#975894238254465104>"
+}
+
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
@@ -20,26 +35,30 @@ client.on("ready", () => {
 client.on("messageCreate", msg => {
     var channelPerms = client.channels.cache.get(msg.channelId).permissionsFor(client.user).toArray()
     
-    // prevents the bot from trying to post in channels where it isn't allowed
+    // prevents the bot from trying to post in channels where it doesn't have permission
     var canSendMessages = channelPerms.includes("SendMessages");
 
     if (!(msg.author.bot) && canSendMessages) {
 
         if (msg.content.toLowerCase().includes("belijah")) {
-            msg.reply("Yes <:Panlijah:1005333329660362762>")
+            msg.reply(`Yes ${emotes.panlijah}`)
         }
+        
+        else if (msg.content.includes("Ben") || msg.content.includes("BEN")) {
+            msg.reply(emotes.panlijah)
+        }
+        
+        // responds when he is mentioned
         else if (
             (msg.content.toLowerCase().includes("elijah")) ||
-            (msg.content.includes("<@1024409224400351274>")) ||
-            (msg.content.includes("<@&1024472122896167018>")) ||
-            (msg.content.includes("<@&1025084954352291950>"))
-
+            (msg.content.includes(roles.self)) ||
+            (msg.content.includes(roles.elijah)) ||
+            (msg.content.includes(roles.bensBoyfriend))
         ) {
             msg.reply("That's me!")
         }
-        else if (msg.content.includes("Ben") || msg.content.includes("BEN")) {
-            msg.reply("<:Panlijah:1005333329660362762>")
-        }
+
+        // responds to mentions of debate with his catchphrase
         else if (msg.content.toLowerCase().includes("debating time")) {
             msg.reply("That's my line!")
         }
@@ -51,7 +70,7 @@ client.on("messageCreate", msg => {
 
 // sends a welcome message to new members that join the server
 client.on('guildMemberAdd', member => {
-    client.channels.cache.get(member.guild.systemChannelId).send(`Ayo <@${member.user.id}>, welcome to the Fiction Fortress! Don't forget to read the rules in <#968645464457101362> and check out <#975894238254465104> to get some roles!`)
+    client.channels.cache.get(member.guild.systemChannelId).send(`Ayo <@${member.user.id}>, welcome to the Fiction Fortress! Don't forget to read the rules in ${channels.rules} and check out ${channels.roles} to get some roles!`)
 })
 
 client.login(process.env.DISCORD_TOKEN)
