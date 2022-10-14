2aaad8009471fcf0f1bc026baa60d340708887d8
felineoverlord <felineoverlord42@gmail.com>
2022-10-13 21:08:40 -0700

    add check for channel permissions before posting

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
+// this line is required for when I run the bot locally for debugging
 // require('dotenv').config();
-const { GatewayIntentBits } = require('discord.js');
+
+const { GatewayIntentBits} = require('discord.js');
 const Discord = require("discord.js");
 const client = new Discord.Client({
     intents: [
@@ -14,10 +16,14 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
+// responds to users' (not bots') messages with various behaviors
 client.on("messageCreate", msg => {
-    // console.log(msg);
+    var channelPerms = client.channels.cache.get(msg.channelId).permissionsFor(client.user).toArray()
+    
+    // prevents the bot from trying to post in channels where it isn't allowed
+    var canSendMessages = channelPerms.includes("SendMessages");
 
-    if (!(msg.author.bot)) {
+    if (!(msg.author.bot) && canSendMessages) {
 
         if (msg.content.toLowerCase().includes("belijah")) {
             msg.reply("Yes <:Panlijah:1005333329660362762>")
@@ -43,6 +49,7 @@ client.on("messageCreate", msg => {
     }
 })
 
+// sends a welcome message to new members that join the server
 client.on('guildMemberAdd', member => {
     client.channels.cache.get(member.guild.systemChannelId).send(`Ayo <@${member.user.id}>, welcome to the Fiction Fortress! Don't forget to read the rules in <#968645464457101362> and check out <#975894238254465104> to get some roles!`)
 })
